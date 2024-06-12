8a85e6e984f34282072158835ee6425502bae470
Serhii Iov <gresvoi.developer@gmail.com>
2024-06-12 17:24:52 +0300

    Fix

--- views/faq.php
+++ views/faq.php
@@ -16,7 +16,7 @@
                 <div class="faq-container">
 
                     <div class="faq-item">
-                        <h3><span class="num"><?php echo $db->GetText('FAQ_1_TITLE')?></h3>
+                        <h3><span class="num"> 1.</span> <span> <?php echo $db->GetText('FAQ_1_TITLE')?></span></h3>
                         <div class="faq-content">
                             <p>
 
@@ -27,7 +27,8 @@
                     </div><!-- End Faq item-->
 
                     <div class="faq-item">
-                        <h3><span class="num"> <?php echo $db->GetText('FAQ_2_TITLE')?> </span></h3>
+
+                        <h3><span class="num"> 2. </span> <span><?php echo $db->GetText('FAQ_2_TITLE')?></span></h3>  </span></h3>
 
                         <div class="faq-content">
                             <p>
@@ -38,7 +39,7 @@
                     </div><!-- End Faq item-->
 
                     <div class="faq-item">
-                        <h3><span class="num"> <?php echo $db->GetText("FAQ_3_TITLE")?></h3>
+                        <h3><span class="num">3.</span> <span> <?php echo $db->GetText("FAQ_3_TITLE")?></span></h3>
                         <div class="faq-content">
                             <p>
                                 <?php
@@ -51,7 +52,7 @@
                     </div><!-- End Faq item-->
 
                     <div class="faq-item">
-                        <h3><span class="num"> <?php echo $db->GetText('FAQ_4_TITLE');?></h3>
+                        <h3><span class="num">4.</span> <span> <?php echo $db->GetText('FAQ_4_TITLE');?></span></h3>
                         <div class="faq-content">
                             <p>
                                 <?php
@@ -63,7 +64,7 @@
                     </div><!-- End Faq item-->
 
                     <div class="faq-item">
-                        <h3><span class="num"> <?php echo $db->GetText('FAQ_5_TITLE')?></h3>
+                        <h3><span class="num">5.</span> <span> <?php echo $db->GetText('FAQ_5_TITLE')?></span></h3>
                         <div class="faq-content">
                             <p>
                                 <?php
